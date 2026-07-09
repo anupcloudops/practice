4773da67c9945471e77f6ec254e21b0c655cef5b
sonalkawale3 <sonalkawale3@gmail.com>
2026-07-09 15:25:56 +0530

    canada code

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "azurerm_virtual_network" "vnet"{
     location = "central India"
     resource_group_name = azurerm_resource_group.rgblock["rg1"].name
 }
+
 resource "azurerm_resource_group" "canada" {
     name = "canada"
     location = "canada central"
